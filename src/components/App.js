2ca45376f74dcf146ea08b16fc8b5ca4d18a327d
jasonk <jasonkhalili@gmail.com>
2020-12-14 23:28:39 -0800

    added react-select package

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Drivers from './Drivers';
-import Seasons from './Seasons';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <Drivers />
-        <Seasons />
       </div>
     )
   }
--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import axios from 'axios';
+import Select from 'react-select';
+
+const options = [
+  { value: 'Lewis Hamilton', label: 'Lewis Hamilton' },
+  { value: 'Max Verstappen', label: 'Max Verstappen' },
+  { value: 'George Russell', label: 'George Russel' }
+];
 
 export default class Drivers extends React.Component {
   state = {
-    drivers: []
-  }
+    drivers: [],
+    selectedOption: null,
+  };
+  handleChange = selectedOption => {
+    this.setState(
+      { selectedOption }
+    );
+  };
 
   componentDidMount() {
-    axios.get('http://ergast.com/api/f1/drivers.json')
+    axios.get('http://ergast.com/api/f1/2020/drivers.json')
     .then(res => {
       const drivers = res.data.MRData.DriverTable.Drivers;
       this.setState({ drivers });
@@ -15,13 +28,22 @@ export default class Drivers extends React.Component {
   }
 
   render() {
+    const { selectedOption } = this.state;
+
     return (
-      <ul>
-        { this.state.drivers.map( e =>
-          <li key={ e.familyName+e.givenName }>
-            { e.givenName } { e.familyName }
-          </li>) }
-      </ul>
-    )
+      <React.Fragment>
+        <Select
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={options}
+        />
+        <ul>
+          { this.state.drivers.map( e =>
+            <li key={ e.familyName+e.givenName }>
+              { e.givenName } { e.familyName }
+            </li>) }
+        </ul>
+      </React.Fragment>
+    );
   }
 }
